dc2fb28b0b8a53b72b6cef50770bfbcf6eff36b3
dylansmall6 <dsmall105@gmail.com>
2017-10-09 11:05:28 -0700

    updated config.js

--- Twitter Bot/app.js
+++ Twitter Bot/app.js
@@ -1,12 +1,7 @@
 var Twit = require('Twit')
+var data = require('./config')
 
-var bot = new Twit({
-    consumer_key: 'lLDHC51xKocDAMt1garJiSF49',
-    consumer_secret: 'WpYpciIV2ZBSBqWsWUElrk6pzI9kDdXsxcPjSw9K4JCLAmjFbv',
-    access_token: '917387499487539202-93JydWgvupWuEx8TzRUWIzJ29QatIeN',
-    access_token_secret: 'BLXyZ6QBiTiTNK7bugDKqRWIxdtzvmRUGQMdEbSkz1O5g',
-    timeout_ms: 60 * 1000,
-})
+var bot = new Twit(data)
 
 setInterval(tweetNumber, 1000 * 12);
 
--- Twitter Bot/config.js
+++ Twitter Bot/config.js
@@ -1,1 +1,8 @@
-// JavaScript source code
+module.exports = {
+    
+    consumer_key: 'lLDHC51xKocDAMt1garJiSF49',
+    consumer_secret: 'WpYpciIV2ZBSBqWsWUElrk6pzI9kDdXsxcPjSw9K4JCLAmjFbv',
+    access_token: '917387499487539202-93JydWgvupWuEx8TzRUWIzJ29QatIeN',
+    access_token_secret: 'BLXyZ6QBiTiTNK7bugDKqRWIxdtzvmRUGQMdEbSkz1O5g',
+    timeout_ms: 60 * 1000,
+}
